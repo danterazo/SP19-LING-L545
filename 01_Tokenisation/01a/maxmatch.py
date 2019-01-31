823d5055786d44a483057a8eb90d43c16f36a337
Dante Razo <dante@razonet.org>
2019-01-31 13:13:30 -0500

    Add file reading capabilities and NLTK sandbox

--- 01_Tokenisation/01a/maxmatch.py
+++ 01_Tokenisation/01a/maxmatch.py
@@ -1,3 +1,27 @@
 # LING-L 445 - Practical 01a
 # Dante Razo, drazo
-print("testing")
+from nltk.tokenize import sent_tokenize
+
+
+def maxmatch(wiki, words):
+    fifty = wiki.read().strip("<onlyinclude>").split(":\n")[1:50]
+
+    print("file:", fifty)
+
+
+def nltk():
+    text = "testing. testing. testing!"
+    sent_tokenize_list = sent_tokenize(text)
+    len(sent_tokenize_list)
+    sent_tokenize_list
+    # > import nltk
+    # > nltk.download()
+
+
+if __name__ == "__main__":
+    wiki = open("wiki.txt", "r", encoding="utf8")
+    words = open("wiki.words", "r", encoding="utf8")
+
+    tokens = maxmatch(wiki, words)
+    print(tokens)
+    nltk()
