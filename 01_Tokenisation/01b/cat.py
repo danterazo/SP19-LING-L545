f9eacea93db51fd170b3a8af87f911a459a9e7cb
Dante Razo <dante@razonet.org>
2019-02-01 07:41:33 -0500

    Update cat command. Unicode printing coming soon

--- 01_Tokenisation/01b/cat.py
+++ 01_Tokenisation/01b/cat.py
@@ -1,7 +1,12 @@
+"""
+This is my program that acts like
+the Unix 'cat' command
+"""
 import sys
 
 # Read in everything from standard input
 text = sys.stdin.read()
 
 # Output everything to standard output
-sys.stdout.write(text)
+#sys.stdout.write(text) #TODO: unicode output
+print(text)
