277f5275e60d2b476a2f19b59a49ea5861731e25
Dante Razo <dante@razonet.org>
2019-02-05 18:39:26 -0500

    Count # of lines, tokens and characters in the input. Line count and Character count mirror unix wc output. Word count WIP

--- 01_Tokenisation/01b/wc.py
+++ 01_Tokenisation/01b/wc.py
@@ -1,8 +1,20 @@
 import sys
 
-wc = 0
 
-for c in sys.stdin.read():
-    wc += 1
-    
-print(wc)
+def wc():
+    # returns (line count, word count, character count
+    lc, wc, cc = 0, 0, 0
+
+    # counting; space delimiter for words
+    for c in sys.stdin.read():
+        cc += 1
+        if c is '\n':
+            lc += 1
+        elif c is ' ':
+            wc += 1
+
+    return lc, wc, cc
+
+
+if __name__ == "__main__":
+    print(wc())
