29028851dc4a26a36b393f755e0161db1a85ff44
Dante Razo <dante@razonet.org>
2019-02-05 18:53:03 -0500

    Further experimenting with word count + delimiters. Added placeholder syllable count int and return

--- 01_Tokenisation/01b/wc.py
+++ 01_Tokenisation/01b/wc.py
@@ -3,18 +3,21 @@ import sys
 
 def wc():
     # returns (line count, word count, character count
-    lc, wc, cc = 0, 0, 0
+    lc, wc, cc, sc = 0, 0, 0, 0
 
     # counting; space delimiter for words
     for c in sys.stdin.read():
         cc += 1
+
         if c is '\n':
             lc += 1
         elif c is ' ':
             wc += 1
 
-    return lc, wc, cc
+    return lc, wc, cc, sc
 
+
+# TODO: syllable count
 
 if __name__ == "__main__":
     print(wc())
