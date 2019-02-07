f3a8cb4db9df2d4905752c8be2c94c6c6d36460a
Dante Razo <dante@razonet.org>
2019-02-07 14:08:45 -0500

    Segments string input. Text file segmentation WIP

--- 01_Tokenisation/01b/segmenter.py
+++ 01_Tokenisation/01b/segmenter.py
@@ -1,1 +1,17 @@
-import sys
+import sys, codecs, fileinput
+
+
+def segmenter():
+    print(sys.stdin.read().replace('. ', '.\n'))
+
+
+def segmenterRU():
+    corpus = open("russian.txt", "r")
+    output = open("russian-segmented.txt", "w")
+
+    output.write(corpus.read().replace('. ', '.\n'))
+    # russian encoding: cp1251
+
+
+if __name__ == "__main__":
+    segmenterRU()
