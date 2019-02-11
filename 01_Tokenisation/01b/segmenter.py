8eefb7e3e0c53258e7fe187ef8d6d617bd091d88
Dante Razo <dante@razonet.org>
2019-02-11 02:09:11 -0500

    New segmenter behavior: if no input is given, segment the included Russian corpus. If input is given (e.g. echo or cat), then segment that.

--- 01_Tokenisation/01b/segmenter.py
+++ 01_Tokenisation/01b/segmenter.py
@@ -9,10 +9,13 @@ def segmenterRU():
     corpus = codecs.open("russian.txt", "r", encoding="cp1251")  # russian encoding: cp1251
     output = codecs.open("russian-segmented.txt", "w", encoding="cp1251")  # create new file for output
 
-    output.write(corpus.read().replace('.', '. \n'))
+    output.write(corpus.read().replace('.', '. \n'))  # NOTE: doesn't work with '. ', unsure why
     corpus.close()
     output.close()
 
 
 if __name__ == "__main__":
-    segmenterRU()
+    if not sys.stdin.isatty():
+        segmenter()
+    else:
+        segmenterRU()
