3e9f836e867e2ed29160958d24b7399ea3a45ef1
Dante Razo <dante@razonet.org>
2019-02-11 01:48:25 -0500

    Segment Russian corpus, but inserts an unwanted and strange character when using current encoding. TODO: clean code, remove unwanted character, and replace '. ' instead of just '.'

--- 01_Tokenisation/01b/segmenter.py
+++ 01_Tokenisation/01b/segmenter.py
@@ -6,11 +6,22 @@ def segmenter():
 
 
 def segmenterRU():
-    corpus = open("russian.txt", "r")
-    output = open("russian-segmented.txt", "w")
+    corpus = codecs.open("russian.txt", "r", encoding="cp1251")  # russian encoding: cp1251
+    codecs.open("russian-segmented.txt", "w").writelines(
+        [l for l in open("russian.txt").readlines()])  # copy input -> output
 
-    output.write(corpus.read().replace('. ', '.\n'))
+    # segment new file by replacing full stops with newlines
+    with fileinput.FileInput("russian-segmented.txt", inplace=True) as file:
+        for line in file:
+            print(line.replace('.', '.\n'), end='')  # TODO: doesn't work when replacing '. '; using temp fix
+
+    # print(output.read().replace('. ', '.\n'))
+    # output.write(output.read().replace('. ', '.\n'))
     # russian encoding: cp1251
+
+    corpus.close()
+
+    # CLOSE files
 
 
 if __name__ == "__main__":
